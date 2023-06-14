832b7e2a34350d75dc9ce756b6230980aa24fdb6
Kemane Donfack <kemanedonfack5@gmail.com>
2023-06-14 14:22:07 +0100

    remove sonarqube

--- main.tf
+++ main.tf
@@ -12,18 +12,18 @@ resource "aws_instance" "jenkins_instance" {
   }
 }
 
-resource "aws_instance" "sonarqube_server" {
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  subnet_id              = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.sonarqube-sg.id]
-  key_name               = var.key_name
-  user_data              = file("install_script_sonarqube.sh")
+# resource "aws_instance" "sonarqube_server" {
+#   ami                    = var.ami
+#   instance_type          = var.instance_type
+#   subnet_id              = aws_subnet.public_subnet.id
+#   vpc_security_group_ids = [aws_security_group.sonarqube-sg.id]
+#   key_name               = var.key_name
+#   user_data              = file("install_script_sonarqube.sh")
 
-  tags = {
-    Name = "Sonarqube"
-  }
-}
+#   tags = {
+#     Name = "Sonarqube"
+#   }
+# }
 
 #Create S3 bucket for your application Artifacts not open to the public
 resource "aws_s3_bucket" "jenkins_bucket" {
@@ -33,41 +33,4 @@ resource "aws_s3_bucket" "jenkins_bucket" {
     name        = "jenkins_bucket"
   }
 }
-
-# resource "aws_db_subnet_group" "database_subnet" {
-#   name       = "db subnet"
-#   subnet_ids = [aws_subnet.database_private_subnet.id, aws_subnet.database_read_replica_private_subnet.id]
-# }
-
-# resource "aws_db_instance" "instance_db1" {
-#   identifier             = "mysql-db1"
-#   db_name                = "mydb"
-#   allocated_storage      = 10
-#   availability_zone      = var.availability_zone[2]
-#   db_subnet_group_name   = aws_db_subnet_group.database_subnet.id
-#   engine                 = "mysql"
-#   engine_version         = "8.0.31"
-#   instance_class         = "db.t3.micro"
-#   multi_az               = false
-#   username               = "username"
-#   password               = "passwordusername"
-#   skip_final_snapshot    = true
-#   vpc_security_group_ids = [aws_security_group.database-sg.id]
-# }
-
-# resource "aws_db_instance" "instance_db2" {
-#   identifier             = "mysql-db2"
-#   db_name                = "mydb"
-#   allocated_storage      = 10
-#   availability_zone      = var.availability_zone[1]
-#   db_subnet_group_name   = aws_db_subnet_group.database_subnet.id
-#   engine                 = "mysql"
-#   engine_version         = "8.0.31"
-#   instance_class         = "db.t3.micro"
-#   multi_az               = false
-#   username               = "username"
-#   password               = "passwordusername"
-#   skip_final_snapshot    = true
-#   vpc_security_group_ids = [aws_security_group.database-sg.id]
-# }
 
--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
 output "jenkins_public_ip" {
   value = aws_instance.jenkins_instance.public_ip
 }
-
-output "sonarqube_public_ip" {
-  value = aws_instance.sonarqube_server.public_ip
-}

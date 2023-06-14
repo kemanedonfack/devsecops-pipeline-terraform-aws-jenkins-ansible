ca5e3c9e89165ac5947a9b70ff5bb77304ea8625
Kemane Donfack <kemanedonfack5@gmail.com>
2023-06-14 14:21:24 +0100

    trivy installation

--- install_script_jenkins_instance.sh
+++ install_script_jenkins_instance.sh
@@ -45,3 +45,10 @@ sudo apt update
 sudo apt install software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+
+#install trivy
+sudo apt-get install wget apt-transport-https gnupg lsb-release -y
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
+sudo apt-get update
+sudo apt-get install trivy

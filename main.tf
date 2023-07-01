14364717e6bfb61b0821eeaaa7f53cf15fe03e18
Kemane Donfack <kemanedonfack5@gmail.com>
2023-07-01 23:23:30 +0100

    add RDS database

--- main.tf
+++ main.tf
@@ -40,6 +40,33 @@ resource "aws_s3_bucket" "jenkins_bucket" {
   }
 }
 
+resource "aws_db_subnet_group" "database_subnet" {
+  name       = "db subnet"
+  subnet_ids = [aws_subnet.database_private_subnet.id, aws_subnet.database_read_replica_private_subnet.id]
+}
+
+resource "aws_db_instance" "rds_master" {
+  identifier              = "master-rds-instance"
+  allocated_storage       = 10
+  engine                  = "mysql"
+  engine_version          = "5.7.37"
+  instance_class          = "db.t3.micro"
+  db_name                 = "db_assiduite"
+  username                = var.db_user
+  password                = var.db_password
+  backup_retention_period = 7
+  multi_az                = false
+  availability_zone       = var.availability_zone[2]
+  db_subnet_group_name    = aws_db_subnet_group.database_subnet.id
+  skip_final_snapshot     = true
+  vpc_security_group_ids  = [aws_security_group.database-sg.id]
+  storage_encrypted       = true
+
+  tags = {
+    Name = "my-rds-master"
+  }
+}
+
 resource "aws_ecr_repository" "ecr" {
   name                 = "assiduite"
   image_tag_mutability = "MUTABLE"
